eb0a4bd5354ab40c371e27f5f27882db0996fe03
huoran <git@huoran.com>
2019-06-09 16:23:15 +0800

    同步异步读写文件

---
+++ 读写文件/readWrite.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+
+// 同步读取
+// 读取文件，在当前的目录下，以utf8格式读取
+var readMe = fs.readFileSync("readMe.txt", "utf8");
+
+// 写入文件，文件名为writeMe,文件内容hello world
+fs.writeFileSync("writeMe.txt", "hello world");
+
+// 异步读取
+var readMe2 = fs.readFile("readMe.txt", "utf8", function(err, data) {
+  console.log(data);
+});
